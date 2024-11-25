76d863a987b9a60fa147b24dbd2104e67aefa386
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2024-11-25 23:31:28 +0200

    feat(vite.config)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-export default {
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        contacts: resolve(__dirname, "src/contacts.html"),
+        values: resolve(__dirname, "src/values.html"),
+      },
+      output: {
+        chunkFileStore: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+      },
+    },
+    minify: "terser",
+    cssMinify: true,
+    sourcemap: false,
   },
-};
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
+});
